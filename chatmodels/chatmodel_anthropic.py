00585ed9740a43637f7e05a38f1ebd0cdb5ee931
Mani K Kumar <manikantmnnit@email.com>
2025-02-12 14:59:10 -0600

    chatmodel: create chatmodel using anthropic

---
+++ chatmodels/chatmodel_anthropic.py
@@ -0,0 +1,8 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model=ChatAnthropic(model='claude-3-5-sonnet-20241022',temperature=0.2,max_tokens_to_sample=15)
+result=model.invoke("capital of USA is ........")
+print(result.content)
