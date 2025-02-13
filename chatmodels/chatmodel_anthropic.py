5130f3f2d3a756cf398bcc9a51d86da4faa56e52
Mani K Kumar <manikantmnnit@email.com>
2025-02-12 21:28:03 -0600

    chatmodel:create chatmodel using  local hugging face model

--- chatmodels/chatmodel_anthropic.py
+++ chatmodels/chatmodel_anthropic.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model=ChatAnthropic(model='claude-3-5-sonnet-20241022',temperature=0.2,max_tokens_to_sample=15)
+model=ChatAnthropic(model='claude-3-5-sonnet-20241022',temperature=0.2,max_tokens_to_sample=)
 result=model.invoke("capital of USA is ........")
 print(result.content)
